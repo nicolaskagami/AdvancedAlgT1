db02583d7b8609c30a0019b509e4923f7b353ddc
Nicolassk <nicolaskagami@gmail.com>
2015-08-17 16:07:00 -0300

    Adicionando Benchmarks e scripts usados

--- buildScalingTests.sh
+++ buildScalingTests.sh
@@ -5,11 +5,11 @@ TESTS="./Tests"
 MIN_VERTS=16
 CONNECTIVITY=0.4
 i=1
-NUM_TESTS=15
+NUM_TESTS=8
 while [ $i -le $NUM_TESTS ]
 do
     VERTS=$(bc <<< "scale=0;$MIN_VERTS*2^$i")
-    echo "$GENERATION/gen $VERTS $CONNECTIVITY 1>$TESTS/t${i}.gr 2>$TESTS/t${i}.ans"
+    $GENERATION/gen $VERTS $CONNECTIVITY 1>$TESTS/t${i}.gr 2>$TESTS/t${i}.ans
     let "i=i+1"
 done
 
